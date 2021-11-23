ef7554e838fe78ab068816848af64ae62bf4bec2
klongrich <longrichk@gmail.com>
2021-11-22 23:58:26 -0500

    adding

--- src/App.js
+++ src/App.js
@@ -81,9 +81,12 @@ function App() {
 
       try {
         fetch(url, {
-          headers: {
-            'origin': 'https://rarity.tools'
-          },
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer',
         })
           .then(res => res.json())
           .then(rawdata => {
